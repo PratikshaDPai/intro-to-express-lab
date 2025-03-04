17e1e767c3898845630d79a82ed56f1425bade70
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-03 22:15:58 -0800

    Add Exercise 3 Solution

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 });
 // Define the route with a dynamic parameter ":username"
 app.get("/greetings/:username", (req, res) => {
-  const username = req.params;
+  const username = req.params.username;
   const greetings = [
     `Hello there, ${username}!`,
     `What a delight it is to see you, ${username}.`,
@@ -31,6 +31,23 @@ app.get("/roll/:num", (req, res) => {
   const responseMessage = Math.floor(Math.random() * dieNum) + 1;
 
   res.send(responseMessage.toString()); //was facing an error since number was being interpreted as status code
+});
+
+const collectibles = [
+  { name: "shiny ball", price: 5.95 },
+  { name: "autographed picture of a dog", price: 10 },
+  { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
+];
+
+app.get("/collectibles/:index", (req, res) => {
+  const index = req.params.index;
+  if (!validator.isInt(index, { min: 0, max: collectibles.length - 1 })) {
+    res.send("This item is not yet in stock. Check back soon!");
+  }
+
+  const responseMessage = `So, you want the ${collectibles[index].name}? For ${collectibles[index].price}, it can be yours!`;
+
+  res.send(responseMessage);
 });
 
 // Start the server on port 3000
